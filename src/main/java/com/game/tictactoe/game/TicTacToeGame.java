45c4d52185497075bca3844f51fcc48d12ef9398
Danila <me@lysunkin.ru>
2024-10-15 17:56:07 +0500

    first commit

---
+++ src/main/java/com/game/tictactoe/game/TicTacToeGame.java
@@ -0,0 +1,65 @@
+package com.game.tictactoe.game;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class TicTacToeGame {
+    private boolean first_turn = true;
+    private final Integer[] cells = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private Integer won;
+
+    public boolean first(int cell) {
+        if (first_turn && won == null) {
+            if (cell > 9 || cell < 1 || cells[cell] == 0) {
+                return false;
+            }
+            cells[cell] = 1;
+            check(1);
+            first_turn = false;
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean second(int cell) {
+        if (!first_turn && won == null) {
+            if (cell > 9 || cell < 1 || cells[cell] == 0) {
+                return false;
+            }
+            cells[cell] = 2;
+            check(2);
+            first_turn = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void check(int player) {
+        for (int i = 1; i < 10; i += 3) {
+            if (cells[i] == player && Objects.equals(cells[i], cells[i + 1]) && Objects.equals(cells[i + 1], cells[i + 2])) {
+                won = player;
+                return;
+            }
+        }
+        for (int i = 1; i <= 3; i++) {
+            if (cells[i] == player && Objects.equals(cells[i], cells[i + 3]) && Objects.equals(cells[i + 3], cells[i + 6])) {
+                won = player;
+                return;
+            }
+        }
+        if (cells[1] == player && Objects.equals(cells[1], cells[5]) && Objects.equals(cells[5], cells[9])) {
+            won = player;
+            return;
+        }
+        if (cells[3] == player && Objects.equals(cells[3], cells[5]) && Objects.equals(cells[5], cells[7])) {
+            won = player;
+        }
+    }
+
+    public Integer getWon() {
+        return won;
+    }
+}
---
+++ src/main/java/com/game/tictactoe/services/GameService.java
@@ -0,0 +1,7 @@
+package com.game.tictactoe.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+}
